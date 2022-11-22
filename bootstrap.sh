ad033a11c05253bbb0380c050bb8e1fe497596c0
bguedes <guedes.bruno@gmail.com>
2022-11-22 15:13:15 +0100

    Adding vagrant files

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ vagrant plugin install vagrant-vbguest
 
 $VAGRANT_EXPERIMENTAL="disks"
 vagrant plugin install vagrant-disksize
+
+cd
+mkdir cdpvm
+cd cdpvm
+
+wget https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/master/vagrant/VMSetup.sh
+wget https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/master/vagrant/Vagrantfile
