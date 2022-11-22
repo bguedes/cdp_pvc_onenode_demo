98af4c1caf091532f97aa1d98f9d9ebcb033a236
bguedes <guedes.bruno@gmail.com>
2022-11-22 21:10:29 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,5 @@ vagrant plugin install vagrant-disksize
 
 vagrant plugin install vagrant-hostmanager
 
-mkdir cdpvm
-cd cdpvm
-
 wget https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/main/vagrant/VMSetup.sh
 wget https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/main/vagrant/Vagrantfile
