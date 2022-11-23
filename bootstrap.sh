6754a92e280421db1f3172cc9ad54432190c320a
bguedes <guedes.bruno@gmail.com>
2022-11-23 14:15:59 +0100

    Adding centos alias

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,6 @@ wget https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/main/vagrant
 
 wget https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --no-check-certificate
 
+vagrant box add CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box --name centos7
+
 chmod +x VMSetup.sh
