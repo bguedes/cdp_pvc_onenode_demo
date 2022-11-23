95249c1768d779c2424d71b56a8b93156f56c43e
bguedes <guedes.bruno@gmail.com>
2022-11-23 18:54:36 +0000

    Adding bootstrap for windows powershell

---
+++ windows_bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+vagrant plugin install vagrant-vbguest
+
+export VAGRANT_EXPERIMENTAL="disks"
+vagrant plugin install vagrant-disksize
+
+vagrant plugin install vagrant-hostmanager
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/main/vagrant/VMSetup.sh -UseBasicParsing -OutFile VMSetup.sh
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/bguedes/cdp_pvc_onenode_demo/main/vagrant/Vagrantfile -UseBasicParsing -OutFile Vagrantfile
+
+Invoke-WebRequest -Uri https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box -UseBasicParsing -OutFile CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box
+
+#chmod +x VMSetup.sh
